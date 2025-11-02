28a1f96d9efd0432324da8b801a349412fa2d7fc
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-11-02 15:48:26 +0000

    new

--- code/1.js
+++ code/1.js
@@ -7,7 +7,7 @@ let accountState;
 console.log(accountId);
 let name = "limon";
 let age = 18;
-let isfalse=false;
+let isfalse = false;
 
 console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
 console.log(typeof age)
@@ -22,3 +22,15 @@ console.log(typeof undefined)
 //null
 //undefined
 //object
+
+console.log("conversion  ")
+let score = "234"; //NULL=>0,undefined=>NaN
+console.log(typeof score)
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+let score1 = "234asd";
+console.log(typeof score)//string
+let valueInNumber1 = Number(score1);
+console.log(valueInNumber1);//NaN
+
+//1=>true ,0=>false,""=>false,"limon"=>true   =>>Boolean conversion
