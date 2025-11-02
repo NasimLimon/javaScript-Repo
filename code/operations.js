0d50c68b3d71f8a75b0b79a2001e911517d28d39
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-11-02 16:16:22 +0000

    new

---
+++ code/operations.js
@@ -0,0 +1,10 @@
+let st1 = "limon"
+let st2 = " Nasim";
+let st3 = st1 + st2;
+console.log(st3)
+console.log("1" + 2);
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log((3 + 4) * 5 % 3)
+console.log(true);
+console.log(+true);
