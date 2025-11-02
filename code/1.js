786c7687daf3c07c33208ff637fdb3413d8366d8
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-11-02 15:16:25 +0000

    new

--- code/1.js
+++ code/1.js
@@ -1,1 +1,24 @@
-console.log("Limon");
+const accountId = "12345";
+let accountEmail = "limon@gmail.com";
+var accountPassword = "654321";//var not used but use let 
+accountCity = "Rangpur";
+let accountState;
+//accountId = "6547";not allowed
+console.log(accountId);
+let name = "limon";
+let age = 18;
+let isfalse=false;
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+//type od data type
+//number
+//string
+//bigint
+//boolean
+//symbol
+//null
+//undefined
+//object
